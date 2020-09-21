9b52f7c87d3b9979f2b1bdc0ca1fba41d9930d49
Nadav23AnT <71144691+Nadav23AnT@users.noreply.github.com>
2020-09-21 15:55:00 +0300

    Create GuessGame.py

---
+++ GuessGame.py
@@ -0,0 +1,40 @@
+from time import sleep
+from Live import load_game
+difficulty = load_game()
+
+
+def _generate_number():
+    # Will generate automatic random number
+    import random
+    secret_number = int(random.uniform(1, difficulty))
+    return secret_number
+
+
+def _get_guess_from_user():
+    # The user will choose a number
+    number = int(input(f"You need to guess a number between 1 to {difficulty}: "))
+    return number
+
+
+def _compare_results(secret_number, number):
+    # checking if the number the user chose equal to the random number
+    if secret_number == number:
+        return True
+
+
+def play():
+    # The start function, will start all functions and return false or true
+    secret_number = _generate_number()
+    print("Generating a Number....")
+    sleep(2)
+    print("Yeap! The number is ready. Now it is Your turn")
+    sleep(1)
+    number = _get_guess_from_user()
+    if _compare_results(secret_number=secret_number, number=number):
+        return True
+    else:
+        return False
+
+
+play()
+# Calling the start module
