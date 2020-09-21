786ab0b685466b25a165ad5512fffaaa0fba991d
Nadav23AnT <71144691+Nadav23AnT@users.noreply.github.com>
2020-09-21 15:55:02 +0300

    Create Live.py

---
+++ Live.py
@@ -0,0 +1,21 @@
+def _welcome(name):
+    # collect the name of the player and prints it
+    name = input('What is your Name? ')
+    print(f'Hello {name} and welcome to the World of Games (WoG) \nHere you can find many cool games to play.')
+
+
+def load_game():
+    # Open the main menu, to the game selection
+    # decision : the game number the player choose
+    # difficulty : the number of difficulty the player choose
+    print("""Please choose a game to play:
+    1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back
+    2. Guess Game - guess a number and see if you chose like the computer
+    3. Currency Roulette - try and guess the value of a random amount of USD in ILS""")
+    decision = int(input("\nchoose between 1-3: "))
+    while 3 < decision or decision < 1:
+        decision = int(input("Please insert a number between 1 - 3: "))
+    difficulty = int(input('Please choose game difficulty from 1 to 5:'))
+    while 5 < difficulty or difficulty < 1:
+        difficulty = int(input("Please insert a number between 1 - 5: "))
+    return difficulty
