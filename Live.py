06136160606ff11cee001b20bb1a5e36056d0e78
Nadav23AnT <71144691+Nadav23AnT@users.noreply.github.com>
2020-09-29 16:13:20 +0300

    Update Live.py

--- Live.py
+++ Live.py
@@ -12,10 +12,16 @@ def load_game():
     1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back
     2. Guess Game - guess a number and see if you chose like the computer
     3. Currency Roulette - try and guess the value of a random amount of USD in ILS""")
-    decision = int(input("\nchoose between 1-3: "))
-    while 3 < decision or decision < 1:
-        decision = int(input("Please insert a number between 1 - 3: "))
-    difficulty = int(input('Please choose game difficulty from 1 to 5:'))
-    while 5 < difficulty or difficulty < 1:
-        difficulty = int(input("Please insert a number between 1 - 5: "))
-    return difficulty
+    while True:
+        try:
+            decision = int(input("Please insert a number between 1 - 3: "))
+            while 3 < decision or decision < 1:
+                decision = int(input("Please insert a number between 1 - 3: "))
+            difficulty = int(input('Please choose game difficulty from 1 to 5:'))
+            while 5 < difficulty or difficulty < 1:
+                difficulty = int(input("Please insert a number between 1 - 5: "))
+        except ValueError:
+            print("Error: Enter just numbers please, not letters, words ,etc...")
+            continue
+
+        return difficulty, decision
