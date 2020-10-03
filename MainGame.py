98aa0c6a07109066af7806c8bf2474dbb54beba9
Nadav Chen <71144691+Nadav23AnT@users.noreply.github.com>
2020-10-03 04:09:03 +0300

    Update MainGame.py

--- MainGame.py
+++ MainGame.py
@@ -1,13 +1,7 @@
 from Live import load_game, _welcome
 
-
 print(_welcome('Guy'))
-difficulty, decision = load_game()
+load_game()
 
-if decision == 1:
-    from GuessGame import play
-    play()
-if decision == 2:
-    from MemoryGame import play
-    play()
+
 
