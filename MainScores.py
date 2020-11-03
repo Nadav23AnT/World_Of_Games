0bcd0ed67b67852684af34e558494b0e22d6e438
Nadav Chen <nadav2282@gmail.com>
2020-11-03 17:30:17 +0200

    Edited MainScores.py using Flask() Added error.html and score.html
     Please enter the commit message for your changes. Lines starting

--- MainScores.py
+++ MainScores.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/")
 def score_server():
-    open(r"E:\MyGitRepo\World_Of_Games\Scores.txt", "r")
-    @app.route("/")
+    try:
+        score = open(r"E:\MyGitRepo\World_Of_Games\scores.txt", "r")
+        return render_template("score.html") # Open score details
+    except FileNotFoundError or FileExistsError: # If the file didn't open for any reason
+        return render_template("error.html") # Prints error message
+
+
+app.run()
