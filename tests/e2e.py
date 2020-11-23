e5c7f85ed7ec3b22159cfff246fdce7784566b4b
Nadav Chen <nadav2282@gmail.com>
2020-11-23 11:01:34 +0200

    Added test score service

---
+++ tests/e2e.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
+
+
+def test_scores_service():
+    driver.get("http://127.0.0.1:5000/")
+    score_element = int(driver.find_element_by_id("score"))
+    if 1000 >= score_element >= 1:
+        return True
+    else:
+        return False
+
+
+def main_function():
+    # The main function will return -1 as an OS exit
+    # code if the tests failed and 0 if they passed.
+    test = test_scores_service()
+    if test:
+        return 0
+    else:
+        return -1
+
