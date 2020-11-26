009bcec3eda35a423ab8e6ab9f8b2dff40636a24
Nadav Chen <nadav2282@gmail.com>
2020-11-26 13:51:17 +0200

    Edited Flask server app

--- MainScores.py
+++ MainScores.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def score_server():
     try:
-        score = open(Path("Scores.txt"), "r")
-        return render_template("score.html", SCORE=score)  # Open score details
+        score_file = open(Path("Scores.tx"), "r")
+        score = score_file.read()
+        return render_template('score.html', SCORE=score)  # Open score details
     except FileNotFoundError or FileExistsError:  # If the file didn't open for any reason
-        return render_template("error.html", ERROR="Unknown Error")  # Prints error message
-
+        return render_template('error.html', ERROR='Unknown Error')  # Prints error message
 
 app.run()
